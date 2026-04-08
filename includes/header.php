d91a0fadf4c2b1cfd2778ade55c8d50038fb6311
flavio <tavaresflavio390@gmail.com>
2026-04-07 23:30:47 -0300

    header e bootstrap

---
+++ includes/header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <title>Sistema Financeiro</title>
+
+    <!-- BOOTSTRAAPPPPP -->
+    https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css
+
+
+</head>
+<body>
+    
+</body>
+</html>
